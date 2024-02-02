17c6e927671eea27e32cb98617c5a5ae32edc947
Anna <134946406+bavovna228@users.noreply.github.com>
2024-02-02 20:08:47 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+const button = document.querySelector('.change-color');
+button.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor(){
+  const color = getRandomHexColor();
+  document.body.style.background = color;
+  
+}
+
