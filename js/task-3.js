caff49e9bc860804817e1cbe1f77a0a4c13743d9
Anna <134946406+bavovna228@users.noreply.github.com>
2024-02-02 16:48:17 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,34 @@
-// // Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) 
+const textInput = document.querySelector('#name-input');
+
+const textOutput = document.querySelector(".output");
+
+
+
+textInput.addEventListener("input", onInputValue);
+
+
+function onInputValue() {
+    const enteredName = textInput.value.trim();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) 
 // підставляє його поточне значення в span#name-output як ім’я для привітання. 
 // Обов’язково очищай значення в інпуті по краях від пробілів . 
 // Якщо інпут порожній або містить лише пробіли, то замість імені у 
 // спан має підставлятися рядок "Anonymous".
 
-// // <input type="text" id="name-input" placeholder="Please enter your name" />
-// // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
