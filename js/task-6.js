2ec10951ab9963ab39f4cfbc9830bf6321fda969
Anna <134946406+bavovna228@users.noreply.github.com>
2024-02-13 12:12:39 +0200

    SUM

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector('#boxes');
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+}
+
+document.querySelector('button[data-create]').addEventListener('click', function() {
+  const input = document.querySelector('input');
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  } 
+});
